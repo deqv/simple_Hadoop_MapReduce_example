c79cfd43c96bfd8cb22b928ed1c244ae875b00e4
Varun Suresh <varunsuresh@protonmail.com>
2024-01-25 16:03:06 +0000

    added two more stopwords

--- mapper.py
+++ mapper.py
@@ -9,6 +9,9 @@ for line in sys.stdin:
     # split the line into words; splits on any whitespace
     words = line.split()
 
+    stopwords = set(['the', 'and'])
+
     # output tuples (word, 1) in tab-delimited format
     for word in words:
         print '%s\t%s' % (word, "1")
+
